712a0ee14e9f6653e43b51153aa3c854ddc23771
Sean McGary <sean.mcgary@gmail.com>
2014-05-19 11:43:51 -0700

    initial commit

---
+++ test/test.js
@@ -0,0 +1,41 @@
+var fancyLog = require('../index');
+
+
+var logger = fancyLog.create({
+	level: fancyLog.levels.DEBUG
+});
+
+logger.log({
+	level: logger.levels.INFO,
+	type: logger.types.SUCCESS,
+	ns: 'api',
+	ident: '123456',
+	message: 'some message',
+	data: { key: 'some value' }
+});
+
+logger.log({
+	level: logger.levels.ERROR,
+	type: logger.types.ERROR,
+	ns: 'api',
+	ident: '123456',
+	message: 'some message',
+	data: { key: 'some value' }
+});
+
+logger.log({
+	level: logger.levels.WARN,
+	ns: 'api',
+	ident: '123456',
+	message: 'some message',
+	data: { key: 'some value' }
+});
+
+logger.log({
+	level: logger.levels.DEBUG,
+	ns: 'api',
+	ident: '123456',
+	message: 'some message',
+	data: { key: 'some value' }
+});
+
